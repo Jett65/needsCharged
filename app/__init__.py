fe3f526287da2af2ce4048157ae0c522c34530af
Jett65 <jettc505@gmail.com>
2023-12-12 18:20:02 -0700

    making it better for a crom job

--- app/__init__.py
+++ app/__init__.py
@@ -1,46 +1,33 @@
 import psutil
+import sqlite3
 import os
-import time
 
-warning_leval = 25
-critical_leval = 15
+conn = sqlite3.connect("needsCharged.db")
+c = conn.cursor()
 
-sound = "sounds/alert.wav"
+warning_leval = 1000
+critical_leval = 1000
+
+battery_leval = int(psutil.sensors_battery().percent)
+is_charging = psutil.sensors_battery().power_plugged
+
+
+def playSound():
+    sound = "../sounds/alert.wav"
+    os.system(f"paplay {sound}")
+
 
 def notify(message, batter_leval, urgency):
     os.system(
         f'notify-send "{message} {int(batter_leval)}%" -u {urgency} -i "🔋"')
 
+if __name__ == "__main__": 
+    if not is_charging:
 
-if __name__ == "__main__":
-    has_notifyed = False
-    notifyed_critical = False
-    while True:
-        battery_leval = int(psutil.sensors_battery().percent)
-        is_charging = psutil.sensors_battery().power_plugged
-
-        if not is_charging:
+            if battery_leval <= warning_leval:
+                notify("Battery Leval Low", battery_leval, "normal")
+                playSound()
 
             if battery_leval <= critical_leval:
-                if not notifyed_critical:
-                    notify("Battery Leval Critical", battery_leval, "critical")
-                    notifyed_critical = True
-                    os.system(f"paplay {sound}")
-
-            else:
-                if not has_notifyed:
-                    if battery_leval <= warning_leval:
-                        notify("Battery Leval Low", battery_leval, "normal")
-                        has_notifyed = True
-                        os.system(f"paplay {sound}")
-
-                    else:
-                        pass
-                else:
-                    pass
-
-        else:
-            has_notifyed = False
-            notifyed_critical = False
-
-        time.sleep(60)
+                notify("Battery Leval Critical", battery_leval, "critical")
+                playSound()
